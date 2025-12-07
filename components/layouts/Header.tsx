6ee936a8f8f3cef2684b87b2953fe9a20b4cc434
juskins <omojuwababatunde1@gmail.com>
2025-12-07 23:47:20 +0100

    Add alx-movie-app

---
+++ components/layouts/Header.tsx
@@ -0,0 +1,5 @@
+const Header: React.FC = () => {
+   return <header>Header Content</header>;
+}
+
+export default Header;
---
+++ components/layouts/Layout.tsx
@@ -0,0 +1,11 @@
+const Layout: React.FC = () => {
+   return (
+      <div>
+         <header>Header Content</header>
+         <main>Main Content</main>
+         <footer>Footer Content</footer>
+      </div>
+   );
+}
+
+export default Layout;
